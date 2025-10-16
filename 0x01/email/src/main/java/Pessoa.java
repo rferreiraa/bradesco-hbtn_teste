58bd04be3b53ba7476f8fc46c2e2aba8e4b6c93f
Rodrigo <rferreiraa02@gmail.com>
2025-10-16 18:18:14 -0300

    teste123

---
+++ 0x01/email/src/main/java/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+
+    public static boolean emailValid(String email) {
+        if (email == null) {
+            return false;
+        }
+
+        return email.contains("@") && email.length() <= 50;
+    }
+}
