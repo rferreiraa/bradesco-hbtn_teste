0e8a6906ff6ed08d8dbdde7116669e12fc261212
Rodrigo <rferreiraa02@gmail.com>
2025-12-02 08:39:08 -0300

    0. Password

--- 0x02/password/src/Person.java
+++ 0x02/password/src/Person.java
@@ -1,10 +1,24 @@
+package src;
+
 public class Person {
 
-    public static boolean emailValid(String email) {
-        if (email == null) {
-            return false;
-        }
+    public boolean checkUser(String username) {
+        if (username == null || username.length() < 8) return false;
 
-        return email.contains("@") && email.length() <= 50;
+        if (!username.matches("[A-Za-z0-9]+")) return false;
+
+        return true;
+    }
+
+    public boolean checkPassword(String password) {
+        if (password == null || password.length() < 8) return false;
+
+        if (!password.matches(".*[A-Z].*")) return false;
+
+        if (!password.matches(".*[0-9].*")) return false;
+
+        if (!password.matches(".*[^A-Za-z0-9].*")) return false;
+
+        return true;
     }
 }
